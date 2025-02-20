31af636788a35b2977f3dee6c2dbf41136dc9524
Gagan41 <gagant.p100@gmail.com>
2025-02-20 11:53:42 +0530

    frontend v 0.2.0

--- forever-full-stack/frontend/src/components/Footer.jsx
+++ forever-full-stack/frontend/src/components/Footer.jsx
@@ -1,45 +1,50 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
     <div>
-      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
-            <img src={assets.logo} className='mb-5 w-32' alt="" />
-            <p className='w-full md:w-2/3 text-gray-600'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Shashi Adda is your go-to e-commerce destination for stylish and
+            high-quality clothing. We offer a carefully curated collection of
+            trendy, comfortable, and elegant apparel designed to suit every
+            occasion. With a focus on premium fabrics and modern designs, we
+            ensure that you always stay ahead in fashion. Shop with us for the
+            latest trends and experience seamless online shopping with great
+            deals and fast delivery!
+          </p>
         </div>
 
         <div>
-            <p className='text-xl font-medium mb-5'>COMPANY</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
-            </ul>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
         </div>
 
         <div>
-            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>+91 73377 41922</li>
-                <li>shashiadda05@gmail.com</li>
-            </ul>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+91 73377 41922</li>
+            <li>shashiadda05@gmail.com</li>
+          </ul>
         </div>
-
       </div>
 
-        <div>
-            <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2025@ shashiadda.com - All Right Reserved.</p>
-        </div>
-
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@ shashiadda.in - All Right Reserved.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- forever-full-stack/frontend/src/pages/Contact.jsx
+++ forever-full-stack/frontend/src/pages/Contact.jsx
@@ -18,8 +18,6 @@ const Contact = () => {
           <p className=' text-gray-500'>BH Road Gubbi <br /> Karnataka,India</p>
           <p className=' text-gray-500'>Ph: +91 73377 41922 <br /> Email: shashiadda05@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
-          <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
       </div>
 
